4fa1486730c7925c246bc6c04bbafbe2dea5c25a
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-07-28 13:04:14 +0000

    Replace scroll eventListener with onclick function. Edit navbar margin

--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-// scrolls to page top on button click
-const scrollTopButton = document.getElementById('scroll-top-button');
-scrollTopButton.addEventListener('click', function () {
-    window.scrollTo(0, 0);
-});
