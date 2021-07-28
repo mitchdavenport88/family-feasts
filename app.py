68a752a2d1f3b4143d35f3f829a71f16bdd51025
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-07-28 08:19:48 +0000

    Secure password confirmation method using werkzeug

--- app.py
+++ app.py
@@ -95,24 +95,24 @@ def register():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        password = request.form.get("password")
-        confirm_password = request.form.get("confirm-password")
+        #  https://techmonger.github.io/4/secure-passwords-werkzeug/
+        #  https://www.youtube.com/watch?v=jJ4awOToB6k
+        password = generate_password_hash(request.form.get("password"))
         if existing_user:
             flash("the username you've chosen is taken!")
             return redirect(url_for("register"))
-        elif password != confirm_password:
-            flash("the passwords entered didn't match!")
-            return redirect(url_for("register"))
-        else:
+        elif check_password_hash(password, request.form.get("confirm-password")):
             register = {
                 "username": request.form.get("username").lower(),
-                "password": generate_password_hash(
-                    request.form.get("password"))
+                "password": password
             }
             mongo.db.users.insert_one(register)
             session["user"] = request.form.get("username").lower()
             flash("registration complete!")
             return redirect(url_for("recipes"))  # Link to profile when made
+        else:
+            flash("the passwords entered didn't match!")
+            return redirect(url_for("register"))
     return render_template("register.html")
 
 
