44325ad81b69932a0c5b3ef9e04281ebde3e41df
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-08-02 17:25:57 +0000

    Add option to show/hide password on login and registration

--- static/js/script.js
+++ static/js/script.js
@@ -17,3 +17,23 @@ function linksToButton(maxScreenWidth) {
 const maxScreenWidth = window.matchMedia("(max-width: 991px)");
 linksToButton(maxScreenWidth);
 maxScreenWidth.addEventListener("change", linksToButton);
+
+// https://www.csestack.org/hide-show-password-eye-icon-html-javascript/
+const togglePassword = document.getElementById('togglePassword');
+const password = document.getElementById('password');
+const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
+const confirmPassword = document.getElementById('confirm-password');
+if (togglePassword) {
+    togglePassword.addEventListener('click', function() {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        this.classList.toggle('fa-eye-slash');
+    });
+}
+if (toggleConfirmPassword) {
+    toggleConfirmPassword.addEventListener('click', function() {
+        const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+        confirmPassword.setAttribute('type', type);
+        this.classList.toggle('fa-eye-slash');
+    });    
+}
