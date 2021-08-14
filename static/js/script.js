5f593da9c9542aa64701c84d0bc54fb82f747539
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-08-14 14:44:34 +0000

    Compress images. Add vendor prefixes. Tidy css and js files

--- static/js/script.js
+++ static/js/script.js
@@ -1,39 +1,19 @@
-// change bootstrap classes for register / login / logout depending on breakpoint
-// code found on https://www.w3schools.com/howto/howto_js_media_queries.asp and edited accordingly
+// Code found on https://www.w3schools.com/howto/howto_js_media_queries.asp and edited accordingly
+/* Changes register / login / logout links to buttons depending on breakpoint */
 function linksToButton(maxScreenWidth) {
     const buttonsToChange = document.getElementsByClassName('link-button');
     if (maxScreenWidth.matches) {
         for (let i = 0; i < buttonsToChange.length; i++ ) {
-            buttonsToChange[i].classList.remove("btn", "my-btn");
-            buttonsToChange[i].classList.add("nav-link");
+            buttonsToChange[i].classList.remove('btn', 'my-btn');
+            buttonsToChange[i].classList.add('nav-link');
         }      
     } else {
         for (let i = 0; i < buttonsToChange.length; i++ ) {
-            buttonsToChange[i].classList.add("btn", "my-btn");
-            buttonsToChange[i].classList.remove("nav-link");
+            buttonsToChange[i].classList.add('btn', 'my-btn');
+            buttonsToChange[i].classList.remove('nav-link');
         }
     }
 }
-const maxScreenWidth = window.matchMedia("(max-width: 991px)");
+const maxScreenWidth = window.matchMedia('(max-width: 991px)');
 linksToButton(maxScreenWidth);
-maxScreenWidth.addEventListener("change", linksToButton);
-
-// https://www.csestack.org/hide-show-password-eye-icon-html-javascript/
-const togglePassword = document.getElementById('togglePassword');
-const password = document.getElementById('password');
-const toggleConfirmPassword = document.getElementById('toggleConfirmPassword');
-const confirmPassword = document.getElementById('confirm-password');
-if (togglePassword) {
-    togglePassword.addEventListener('click', function() {
-        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-        password.setAttribute('type', type);
-        this.classList.toggle('fa-eye-slash');
-    });
-}
-if (toggleConfirmPassword) {
-    toggleConfirmPassword.addEventListener('click', function() {
-        const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password';
-        confirmPassword.setAttribute('type', type);
-        this.classList.toggle('fa-eye-slash');
-    });    
-}
+maxScreenWidth.addEventListener('change', linksToButton);
